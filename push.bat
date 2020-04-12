9fdebd35d03de14ffbb8d06f7a2e5fafbe03eb2b
裘昱 <hf_qy@sohu.com>
2020-04-12 10:40:51 +0800

    2020/04/12-desktop

---
+++ push.bat
@@ -0,0 +1,25 @@
+﻿echo .....Jump to folder E:\架构\学习笔记
+cd /d E:\架构\学习笔记
+
+echo .....!!!!添加要提交的内容
+git add .
+
+echo .....!!!!OK
+
+echo .....!!!!提交本地版本库
+git commit -m  %date:~0,4%/%date:~5,2%/%date:~8,2%-desktop
+
+echo .....!!!!OK
+
+echo .....!!!!提交gitee
+git push gitee
+
+echo .....!!!!提交gitee OK
+
+echo .....!!!!提交github
+git push origin
+
+echo .....!!!!提交github OK
+
+echo .....任意键退出
+pause>nul
